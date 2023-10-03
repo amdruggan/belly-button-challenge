397fbb91808a73f05f652fc377f4a9aa080a13c4
amdruggan <61000582+amdruggan@users.noreply.github.com>
2023-10-03 18:02:41 -0400

    started app.js file

---
+++ static/js/app.js
@@ -0,0 +1,11 @@
+//Use the D3 library to read in samples.json from the URL https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.
+let url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+d3.json(url).then(function(data) {
+    console.log(data);
+});
+
+// Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+// Use sample_values as the values for the bar chart.
+// Use otu_ids as the labels for the bar chart.
+// Use otu_labels as the hovertext for the chart.
