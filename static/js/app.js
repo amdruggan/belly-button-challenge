54f51323e63762bd19700d2a0cf1910c1f7e9e3e
amdruggan <61000582+amdruggan@users.noreply.github.com>
2023-10-05 18:00:12 -0400

    Update app.js
    Added gauge chart and metadata functions

--- static/js/app.js
+++ static/js/app.js
@@ -99,3 +99,103 @@ function BubbleChart(subject) {
 
     });
 }
+
+// Create a function that gets the sample metadata for a given subject ID
+function MetaData(subject) {
+
+    // This reads in the data from the JSON file using D3
+    d3.json(url).then((data) => {
+
+        // This extracts the metadata from the JSON file
+        const metadata = data.metadata;
+
+        // This filters the metadata to only include the data for the given subject ID
+        let resultArray = metadata.filter(subjectMetadata => subjectMetadata.id == subject);
+        let result = resultArray[0];
+
+        // This selects the Demographic Info box
+        let demographicInfo = d3.select("#sample-metadata");
+
+        // This clears the Demographic Info box
+        demographicInfo.html("");
+
+        // This appends the metadata to the Demographic Info box
+        Object.entries(result).forEach(([key, value]) => {
+            demographicInfo.append("h5").text(`${key}: ${value}`);
+        });
+
+    });
+}
+
+// Create a function that gets the washing frequency for a given subject ID, then plots the washing frequency on a plot.ly gauge chart
+function GaugeChart(subject) {
+
+    // This reads in the data from the JSON file using D3 and creates a variable for number of washes
+    d3.json(url).then((data) => {
+
+        // This extracts the metadata from the JSON file
+        const metadata = data.metadata;
+
+        // This filters the metadata to only include the data for the given subject ID
+        let resultArray = metadata.filter(subjectMetadata => subjectMetadata.id == subject);
+        let result = resultArray[0];
+
+        // This creates a variable for the number of washes
+        let washes = result.wfreq;
+
+    });
+
+    // This adds the number of washes to the gauge chart and also inputs al the variables for the gauge chart parameters
+    var GaugeData = [
+        {
+        // Type of gauge chart
+          type: "indicator",
+
+        // Included modes (gauge and number)
+          mode: "gauge+number",
+
+        // Sets the number of washes based on the subject ID (from the D3 read in above)
+          value: washes,
+
+        // Sets the title of the gauge chart
+          title: { text: "Belly Button Washing Frequency", font: { size: 24 } },
+
+        // Sets the range of the gauge chart
+          gauge: {
+            axis: { range: [null, 9], tickwidth: 1, tickcolor: "darkblue" },
+            bar: { color: "darkblue" },
+            bgcolor: "white",
+            borderwidth: 2,
+            bordercolor: "gray",
+            steps: [
+              { range: [0, 1], color: "cyan" },
+              { range: [1, 2], color: "royalblue" },
+              { range: [2, 3], color: "cyan" },
+              { range: [3, 4], color: "royalblue" },
+              { range: [4, 5], color: "cyan" },
+              { range: [5, 6], color: "royalblue" },
+              { range: [6, 7], color: "cyan" },
+              { range: [7, 8], color: "royalblue" },
+              { range: [8, 9], color: "cyan" }
+            ],
+            threshold: {
+              line: { color: "red", width: 4 },
+              thickness: 0.5,
+              value: 9
+            }
+          }
+        }
+      ];
+
+    // This sets the formatting parameters for the gauge chart
+    var GaugeLayout = {
+        width: 500,
+        height: 400,
+        margin: { t: 25, r: 25, l: 25, b: 25 },  
+        paper_bgcolor: "lavender",
+        font: { color: "darkblue", family: "Arial" }
+    };
+
+    // This displays the gauge chart
+    Plotly.newPlot('myDiv', data, layout);
+    }
